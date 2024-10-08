2f4351219bdac3ec6ddcfbb9464e5bbbd150a859
419kfj <kazuo.fujimoto2007@gmail.com>
2024-10-08 14:13:20 +0900

    commit 20241008

--- app.R
+++ app.R
@@ -17,44 +17,24 @@ library(GGally)
 library(DT)
 library(gtsummary)
 library(gt)
+library(showtext)
+showtext_auto(TRUE)
 
 #-------------------------------------------------------------------------------
 # データ読み込みと前処理整形
 
-# Data load from CYDER_ENQ4/data/　← 別のディレクトリからもってくる
-load("../CYDER_ENQ4/data/CYDER2020A.rda")
-CYDER2020A %>% mutate(イン発生=recode_factor(イン発生,"はい（アンケート１１－２にもお答えください）"="はい"),
-                      訓練演習=recode_factor(訓練演習,"定期的な訓練を行っている（アンケート１２−２にもお答えください）"="定期的に行っている",
-                                         "訓練を行ったことがある（アンケート１２−２にもお答えください）"="行ったことがある",
-                                         "今後、訓練を行う予定である（アンケート１２−２にもお答えください）"="今後行う予定",
-                                         "その他（アンケート１２−２にもお答えください）"="その他"),
-                      職業=recode_factor(職業,"市長村公務員"="市町村公務員")) -> CYDER2020A
-
-load("../CYDER_ENQ4/data/CYDER2020B1.rda")
-load("../CYDER_ENQ4/data/CYDER2020B2.rda")
-
-load("../CYDER_ENQ4/data/CYDER2021A.rda")
-CYDER2021A %>% mutate(イン発生=recode_factor(イン発生,"はい（アンケート１１−２にもお答えください）"="はい"),
-                      訓練演習=recode_factor(訓練演習,"定期的な訓練を行っている（アンケート１２−２にもお答えください）"="定期的に行っている",
-                                         "訓練を行ったことがある（アンケート１２−２にもお答えください）"="行ったことがある",
-                                         "今後、訓練を行う予定である（アンケート１２−２にもお答えください）"="今後行う予定",
-                                         "その他（アンケート１２−２にもお答えください）"="その他")) -> CYDER2021A
-load("../CYDER_ENQ4/data/CYDER2021B1.rda")
-load("../CYDER_ENQ4/data/CYDER2021B2.rda")
-
-load("../CYDER_ENQ4/data/CYDER2019A.rda")
-load("../CYDER_ENQ4/data/CYDER2019B1.rda")
-load("../CYDER_ENQ4/data/CYDER2019B2.rda")
+load("./data/iwate.f.mac.rda")
 
 #-------------------------------------------------------------------------------
 # Define UI for application 
 #
-ui <- navbarPage("CYDER受講者アンケート分析",
+ui <- navbarPage("iwate 調査データ簡易集計",
                tabPanel("About",
-                        h1("CYDER受講者の受講後アンケート簡易分析"),
+                        h1("iwateデータを分析する"),
                         h2("アプリケーション概要"),
                         p("基本集計、調査票、関連リンク、を掲載"),
                         helpText("構成上の要望うけつけてます。"),
+                        p("ver1.7 2024/10/07 CYDERデータ分析からiwateデータ分析ように修正"),
                         p("ver1.6 2024/01/30 gitでversion管理を開始、NLP2024論文をLINK"),
 　　　　　　　　　　　　p("ver1.5 2023/10/23 クロス集計にgtsummary::tbl_crossを適用"),
                         p("ver1.0 2023/06/12 プロトタイプから利用可能なレベルにしてリリース"),
@@ -73,32 +53,25 @@ ui <- navbarPage("CYDER受講者アンケート分析",
                #--------------------------------
                tabPanel(
                  "基本集計",
-                 h1("受講者アンケートの基本集計を行います。"),
+#                 h1("分析対象df（.rda）をuploadしてください。"),
+                 h1("iwateデータの基本集計を行います。"),
                  sidebarPanel(
                    selectInput(
                      "selected_data_for_plot",
                      label = h3("データセットを選択してください。"),
-                     choices = c(
-                       "CYDER2019A" = "CYDER2019A",
-                       "CYDER2019B1" = "CYDER2019B1",
-                       "CYDER2019B2" = "CYDER2019B2",
-                       "CYDER2020A" = "CYDER2020A",
-                       "CYDER2020B1" = "CYDER2020B1",
-                       "CYDER2020B2" = "CYDER2020B2",
-                       "CYDER2021A" = "CYDER2021A",
-                       "CYDER2021B1" = "CYDER2021B1",
-                       "CYDER2021B2" = "CYDER2021B2"
-                     ),
-                     selected = "CYDER2020A"
-                   ),
+                     choices = c("iwate" = "iwate.f"), selected = "iwate"),
                    selectInput("select_input_data_for_hist",
                                "集計する変数",
-                               choices = colnames(CYDER2020A)),
+                                choices = colnames(iwate.f),
+                                selected =  colnames(iwate.f)[3]),
                    selectInput("select_input_data_for_cross",
                                "クロス集計する変数",
                                choices = NULL),
+                   selectInput("select_input_data_for_layer",
+                               "層化する変数",
+                               choices = NULL),
                  ),
-                 
+                 #----　MAIN Panel
                  mainPanel(
                    tabsetPanel(type = "tabs",
                                tabPanel("単変数集計",
@@ -114,6 +87,14 @@ ui <- navbarPage("CYDER受講者アンケート分析",
                                         h3("χ2乗検定"),
                                         verbatimTextOutput("chisq_test")
                                ),
+                               # tabPanel("2変数分析（層化）",
+                               #          h2("クロス集計（gtsummary::tbl_cross）"),
+                               #          #  verbatimTextOutput("crosstable"),
+                               #          gt_output(outputId = "my_gt_table"),
+                               #          plotOutput("crosschart2",width = 600, height = 600),
+                               #          h3("χ2乗検定"),
+                               #          verbatimTextOutput("chisq_test")
+                               # ),
                                tabPanel("pairs",
                                         h2("GGally::pairs"),
                                         plotOutput("pairs",width = 600, height = 600)
@@ -123,39 +104,18 @@ ui <- navbarPage("CYDER受講者アンケート分析",
                                         DT::dataTableOutput("table_for_plot")
                                ),
                                tabPanel("自由記述文分析")
-                   ))
+                   )
+              　)
                ),
                #--------------------------------
                tabPanel("調査票",
                         h3("参考資料 PDFはブラウザの設定viewerで開きます"),
+#                       tags$a(href = "http://133.167.73.14/~kazuo/ruda0010-questionnaire.pdf", "PDFを開く", target = "_blank"),
                         tags$iframe(style="height:800px; width:100%; scrolling=yes", #400px
                                     #src="./CYDER_ENQ3/data/CYDER2020_ENQ_20200806.pdf"
                                     #src="./data/CYDER2020_ENQ_20200806.pdf"
-                                    #src="../CYDER_ENQ4/data/CYDER2020_ENQ_20200806.pdf"
-                                    src="http://172.27.10.50/~fujimoto/CYDER2020_ENQ_20200806_A.pdf"
+                                    src="http://133.167.73.14/~kazuo/ruda0010-questionnaire.pdf"
                         ),
-                        HTML("<ul>"),
-                        HTML("<li>"),
-                        a(href = "http://172.27.10.50/~fujimoto/CYDER2019_A_アンケート_SCENARIO000029-1_20190528_v02.pdf", "CYDER2019A調査票"),
-                        HTML("</li>"),
-                        HTML("<li>"),
-                        a(href = "http://172.27.10.50/~fujimoto/CYDER2019_B-1_アンケート_SCENARIO000042_20190725_03_v02.pdf", "CYDER2019B1調査票"),
-                        HTML("</li>"),
-                        HTML("<li>"),
-                        a(href = "http://172.27.10.50/~fujimoto/CYDER2019_B-2_アンケート_SCENARIO000062_20191025_02_v02.pdf", "CYDER2019B2調査票"),
-                        HTML("</li>"),
-                        
-                        HTML("<li>"),
-                        a(href = "http://172.27.10.50/~fujimoto/CYDER2020_ENQ_20200806_A.pdf", "CYDER2020A調査票"),
-                        HTML("</li>"),
-                        HTML("<li>"),
-                        a(href = "http://172.27.10.50/~fujimoto/CYDER2020_共通_受講後アンケート_20201008_B1.pdf", "CYDER2020B1調査票"),
-                        HTML("</li>"),
-                        HTML("<li>"),
-                        a(href = "http://172.27.10.50/~fujimoto/CYDER2020_共通_受講後アンケート_20201008_B2.pdf", "CYDER2020B2調査票"),
-                        HTML("</li>"),
-                        
-                        HTML("</ul>"),
                         helpText("shinyでPDFをviewできるようにします。")
                ),
                tabPanel("関連リンク集",
@@ -214,7 +174,7 @@ ui <- navbarPage("CYDER受講者アンケート分析",
                                    )),
               
                  )# navbarMenu close
-)
+)　# ui navbarPage close
   
 #-------------------------------------------------------------------------------
 # Define server logic required to draw outlut tables and graphs
@@ -233,23 +193,11 @@ server <- function(input, output, session) {
                      "titanic" = data.frame(lapply(data.frame(Titanic), 
                                                    function(i){rep(i, data.frame(Titanic)[, 5])})),
                      "Womenlf" = Womenlf,
-                     #"iwate" = iwate_utf8,
-                     "SRP2018" = SRP2018,
-                     # "CYDER2020A" = cyder, #CYDER2020A_all[,-c(1:9,17,19)] #%>% select(-c(2:9,17,19)),
-                     "CYDER2019A" = CYDER2019A[,-c(1:2)],
-                     "CYDER2019B1" = CYDER2019B1[,-c(1:2)],
-                     "CYDER2019B2" = CYDER2019B2[,-c(1:2)],
-                     "CYDER2020A" = CYDER2020A[,-c(1:2)],
-                     "CYDER2020B1" = CYDER2020B1[,-c(1:2)],
-                     "CYDER2020B2" = CYDER2020B2[,-c(1:2)],
-                     "CYDER2021A" = CYDER2021A[,-c(1:2)],
-                     "CYDER2021B1" = CYDER2021B1[,-c(1:2)],
-                     "CYDER2021B2" = CYDER2021B2[,-c(1:2)],
-                     
-                     "iwate" = iwate
+                     "iwate.f" = iwate.f[,-c(1,2)]
       )
       updateSelectInput(session, "select_input_data_for_hist", choices = colnames(data))
       updateSelectInput(session, "select_input_data_for_cross", choices = c(" ",colnames(data)))
+      updateSelectInput(session, "select_input_data_for_layer", choices = c(" ",colnames(data)))
       return(data)
     })
     # barplot by ggplot2
@@ -267,32 +215,47 @@ server <- function(input, output, session) {
       p
     })  
     # mosaic plot
-
     output$crosschart <- renderPlot({
       .tbl <- table(data_for_plot()[,input$select_input_data_for_cross],
                     data_for_plot()[,input$select_input_data_for_hist]) 
       .tbl.p <- round(100 * prop.table(.tbl ,margin = 1),1)
       tab <- ifelse(.tbl.p < 1, NA, .tbl.p) 
-
-      data_for_plot()[,c(input$select_input_data_for_hist,input$select_input_data_for_cross)] %>% 
-        structable() %>% 
-        mosaic(#main=str_c(input$select_input_data_for_cross,"-",input$select_input_data_for_hist),
-          shade=TRUE,las=2,
-          labeling=labeling_values
-          #pop=FALSE
+      
+      data_for_plot()[,c(input$select_input_data_for_hist,
+                         input$select_input_data_for_cross)] %>% 
+        structable() %>%
+        mosaic(shade=TRUE,las=2,
+               labeling=labeling_values
         )
+    })
+    # 層化mosaic　plot  
+    output$crosschart2 <- renderPlot({
+      .tbl <- table(data_for_plot()[,input$select_input_data_for_cross],
+                    data_for_plot()[,input$select_input_data_for_hist]) 
+      .tbl.p <- round(100 * prop.table(.tbl ,margin = 1),1)
+      tab <- ifelse(.tbl.p < 1, NA, .tbl.p) 
+
+      data_for_plot()[,c(input$select_input_data_for_hist,
+                         input$select_input_data_for_cross,
+                         input$select_input_data_for_layer)] %>% 
+          structable() %>%
+          mosaic(condvars = 3, # input$select_input_data_for_layer を指定
+                 split_vertical = TRUE,# 分割は垂直
+                 shade=TRUE,las=2,
+                 labeling=labeling_values
+          )
+      # data_for_plot()[,c(input$select_input_data_for_hist,input$select_input_data_for_cross)] %>% 
+      #   structable() %>% 
+      #   mosaic(#main=str_c(input$select_input_data_for_cross,"-",input$select_input_data_for_hist),
+      #     shade=TRUE,las=2,
+      #     labeling=labeling_values
+      #     #pop=FALSE
+      #   )
        #labeling_cells(text=tab,clip=FALSE)(as.table(.tbl.p))
     })
-    # クロス表表示
-#    output$crosstable <- renderPrint({
-#      .tbl <- table(data_for_plot()[,input$select_input_data_for_cross],
-#                    data_for_plot()[,input$select_input_data_for_hist])
-#      .tbl
-#    })
-    
+
     # gtsummary でクロス表表示
-    output$my_gt_table <-
-      render_gt(
+    output$my_gt_table <- render_gt(
         data_for_plot() %>% tbl_cross(row = input$select_input_data_for_cross,
                                       col = input$select_input_data_for_hist,
                                       percent = "row") %>% 
@@ -300,11 +263,6 @@ server <- function(input, output, session) {
           bold_labels() %>% 
           as_gt()
       )
-
-    output$ENQANS2018 <- DT::renderDataTable({
-      EnqAns2018
-    })
-    
     output$chisq_test <- renderPrint({
       chisq.test(table(data_for_plot()[,input$select_input_data_for_cross],
                        data_for_plot()[,input$select_input_data_for_hist]))
