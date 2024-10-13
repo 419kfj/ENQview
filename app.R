e7fa81f7141a2e3301b37d7deeb19d3c3c2da8fe
419kfj <kazuo.fujimoto2007@gmail.com>
2024-10-14 07:21:42 +0900

    2024/10/13までの動作版commit

--- app.R
+++ app.R
@@ -119,21 +119,22 @@ ui <- navbarPage("調査データ簡易集計",
                         HTML("<li>"),
                         a(href = "https://www.danieldsjoberg.com/gtsummary/articles/shiny.html",target="_blank", "Shinyの中でgtsummaryを使う"),
                         HTML("</li>"),
-                        HTML("<ul>")
+                        HTML("<ul>"),
+　　　　　　　　　　　　h3("app.R 最終更新日時:"),
+　　　　　　　　　　　　textOutput("timestamp")  # タイムスタンプの表示
                ),
                #--------------------------------
                tabPanel(
                  "基本集計",
 #                 h1("分析対象df（.rda）をuploadしてください。"),
-                 h1("データの基本集計を行います。"),
+                 h1("基本集計を行います。"),
                  sidebarPanel(
-                   selectInput(
-                     "selected_data_for_plot",
-                     label = h3("データセットを選択してください。"),
-                     choices = c("iwate" = "iwate.f",
-                                 "Bunka"="Bunka"#,
-#                                 "Bunka"="BunkaRC"
-                                 ), selected = "Bunka"),
+                   selectInput("selected_data_for_plot",
+                     　　　　　label = h3("集計対象を選択してください。"),
+                               choices = c("iwate" = "iwate.f",
+                                           "Bunka"="Bunka"#,
+          #                                 "Bunka"="BunkaRC"
+                                           ), selected = "Bunka"),
                    selectInput("select_input_data_for_hist",
                                "集計する変数",
                                choices = colnames("selected_data_for_plot"),
@@ -145,7 +146,7 @@ ui <- navbarPage("調査データ簡易集計",
                    selectInput("select_input_data_for_layer",
                                "層化する変数",
                                choices = NULL),
-                   selectInput("variables", "服数変数の選択（MAなど）:", 
+                   selectInput("variables", "変数の複数選択（MAなど）:", 
                                choices =  NULL,
                                multiple = TRUE,
                                selectize = FALSE,
@@ -158,7 +159,7 @@ ui <- navbarPage("調査データ簡易集計",
                  mainPanel(
                    tabsetPanel(type = "tabs",
                                tabPanel("単変数集計",
-                                        h2("基本集計"),
+                                        h2("棒グラフと度数分布"),
                                         plotOutput("barchart"),
                                         DT::dataTableOutput("simple_table"), # server.R も対応させること！
                                ),
@@ -170,11 +171,15 @@ ui <- navbarPage("調査データ簡易集計",
                                         h3("χ2乗検定"),
                                         verbatimTextOutput("chisq_test")
                                ),
+                               tabPanel("pairs",
+                                        h2("GGally::pairs"),
+                                        plotOutput("pairs",width = 600, height = 600)
+                               ),
                                tabPanel("2変数分析（層化）",
                                         h2("クロス集計（gtsummary::tbl_cross）"),
                                         #  verbatimTextOutput("crosstable"),
                                         gt_output(outputId = "my_gt_table"),
-                                        plotOutput("crosschart2",width = 600, height = 600),
+                                        plotOutput("crosschart2",width = 900, height = 600),
                                         h3("χ2乗検定"),
                                         verbatimTextOutput("chisq_test")
                                ),
@@ -194,6 +199,14 @@ ui <- navbarPage("調査データ簡易集計",
                                         plotOutput("MAplot_lineDotwarp",width = 600, height = 600)
                                ),
                                
+                               tabPanel("Grid回答 General mosaic表示",
+                                        h2("Grid回答mosaic表示"),
+                                        plotOutput("GridAnswerG_mosaic",width = 600, height = 600),
+                                        plotOutput("GridAnswerG_CA",width = 700, height = 700)
+                               ),
+                               
+                               
+                               
                                tabPanel("Grid回答++/-- mosaic表示",
                                         h2("Grid回答mosaic表示"),
                                         plotOutput("GridAnswer_mosaic",width = 600, height = 600),
@@ -203,13 +216,11 @@ ui <- navbarPage("調査データ簡易集計",
                                
                                tabPanel("Grid回答 LK/DLK mosaic表示",
                                         h2("Grid回答mosaic表示"),
-                                        plotOutput("GridAnswer2_mosaic",width = 600, height = 600)
+                                        plotOutput("GridAnswer2_mosaic",width = 600, height = 600),
+                                        plotOutput("GridAnswer2_CA",width = 700, height = 700)
                                ),
                                
-                               tabPanel("pairs",
-                                        h2("GGally::pairs"),
-                                        plotOutput("pairs",width = 600, height = 600)
-                               ),
+                            
                                tabPanel("データ一覧",
                                         h2("データ一覧"),
                                         DT::dataTableOutput("table_for_plot")
@@ -227,11 +238,24 @@ ui <- navbarPage("調査データ簡易集計",
 #                                     #src="./data/CYDER2020_ENQ_20200806.pdf"
 #                                     src="http://133.167.73.14/~kazuo/ruda0010-questionnaire.pdf"
 #                         ),
+HTML("<ul>"),
+HTML("<li>"),
                          a(href = "http://133.167.73.14/~kazuo/BunkaQues.pdf", "「文化と不平等」調査調査票",target = "_blank"),
-
-#                        a(href = "http://133.167.73.14/~kazuo/ruda0010-questionnaire.pdf", "岩手調査調査票",target = "_blank"),
-                        helpText("別TabでPDFが開きます")
+HTML("</li>"),
+HTML("<li>"),
+                        a(href = "http://133.167.73.14/~kazuo/ruda0010-questionnaire.pdf", "岩手調査調査票",target = "_blank"),
+HTML("</li>"),
+HTML("</ul>")
+#                        helpText("別TabでPDFが開きます")
                ),
+
+              tabPanel("使い方解説",
+                       a(href = "http://133.167.73.14/~kazuo/Tool_help.pdf", "使い方解説：別タブで開きます",target = "_blank"),
+                       
+                       helpText("別TabでPDFが開きます")
+              ),
+
+
                tabPanel("関連リンク集",
                         HTML("<ul>"),
                         HTML("<li>"),
@@ -484,21 +508,23 @@ server <- function(input, output, session) {
 
 # Grid Mosaic
 # 
-    
+
     output$GridAnswer_mosaic <- renderPlot({
-      selected_vars <- input$variables 
+      selected_vars <- input$variables
       count_categories <- function(x) {
         table(factor(x, levels = c("++", "+", "-", "--", "DK", "無回答"), exclude = NULL))
       }
-      
+
       # df の 1 列目から 20 列目の各列ごとにカテゴリを集計
       category_count_tbl <- data_for_plot() %>%
-        dplyr::summarise(across(selected_vars, ~ count_categories(.))) 
-      
+        dplyr::summarise(across(selected_vars, ~ count_categories(.)))
+
       category_count_tbl %>% as.matrix() -> cat_tbl
       rownames(cat_tbl) <- c("++","+","-","--","DK","無回答")
       cat_tbl
-      
+  
+#      data_tbl <- Grid_table()
+#      cat_tbl <- data_tbl          
       rownames(t(cat_tbl)) -> rnames
       t(cat_tbl) %>% as.tibble() %>% mutate(ID=rnames,IDn=1:length(rnames)) %>% 
         mutate(Like=`++`+`+`) %>% 
@@ -506,9 +532,9 @@ server <- function(input, output, session) {
         select(IDn) %>% unlist %>% 
         setNames(NULL) -> order_vec
       
-#      t(cat_tbl)[order_vec,]
-      t(cat_tbl) %>% mosaic(shade = TRUE,rot_labels = c(0, 0),
-                                        margins=c(left=9,top=5),just_labels=c(left="right",top="left"))
+      t(cat_tbl)[order_vec,] %>% 
+        mosaic(shade = TRUE,rot_labels = c(0, 0),
+               margins=c(left=9,top=5),just_labels=c(left="right",top="left"))
       
     })
 
@@ -533,14 +559,12 @@ server <- function(input, output, session) {
     })
       
 # Grid Mosaic 2 LK/DLK
-    # 
-　　
-        
+    
     output$GridAnswer2_mosaic <- renderPlot({
-      grid_ptn <- c("A","B","C","D","E","NA") 
+      grid_ptn <- c("A","B","C","D","E","NA") # 仮のパターン
       selected_vars <- input$variables
       count_categories <- function(x) {
-        table(factor(x, levels = grid_ptn,#c("++", "+", "-", "--", "DK", "無回答"),
+        table(factor(x, levels = grid_ptn,
                      exclude = NULL))
       }
       
@@ -549,7 +573,7 @@ server <- function(input, output, session) {
         dplyr::summarise(across(selected_vars, ~ count_categories(.))) 
       
       category_count_tbl %>% as.matrix() -> cat_tbl
-      rownames(cat_tbl) <- grid_ptn #c("++","+","-","--","DK","無回答")
+      rownames(cat_tbl) <- grid_ptn 
       cat_tbl
       
       rownames(t(cat_tbl)) -> rnames
@@ -564,8 +588,124 @@ server <- function(input, output, session) {
                                         margins=c(left=9,top=5),just_labels=c(left="right",top="left"))
       
     })
+    
+    
+# GridAnswer2 CA
+    output$GridAnswer2_CA <- renderPlot({
+      grid_ptn <- c("A","B","C","D","E","NA") # 仮のパターン
+      selected_vars <- input$variables 
+      count_categories <- function(x) {
+        table(factor(x, levels = grid_ptn,#c("++", "+", "-", "--", "DK", "無回答"), 
+                     exclude = NULL))
+      }
+      
+      # df の 1 列目から 20 列目の各列ごとにカテゴリを集計
+      category_count_tbl <- data_for_plot() %>%
+        dplyr::summarise(across(selected_vars, ~ count_categories(.))) 
+      
+      category_count_tbl %>% as.matrix() -> cat_tbl
+      rownames(cat_tbl) <- grid_ptn #c("++","+","-","--","DK","無回答")
+      cat_tbl
+      
+      res.CA <- FactoMineR::CA(t(cat_tbl))
+      
+    })
 
-    # 度数分布表
+    
+    # Grid Mosaic Genenral
+    
+    output$GridAnswerG_mosaic <- renderPlot({
+
+      selected_vars <- input$variables
+      vectors <- map(selected_vars, ~ {
+　        data_for_plot() %>% select(selected_vars) %>% 
+          count(!!sym(.x)) %>%  # 選択した列ごとにカウント
+          pull(1)               # 最初の列のユニークな値を取得
+      })
+      
+      union_all <- reduce(vectors, union)
+      union_all <- ifelse(is.na(union_all), "NA", union_all)
+      count_categories <- function(x) {
+        table(factor(x, levels = union_all, exclude =NULL))
+      }
+      
+      # df のselected_varsの各列ごとにカテゴリを集計
+      category_count_tbl <- data_for_plot() %>%
+        dplyr::summarise(across(selected_vars, ~ count_categories(.))) 
+      
+      category_count_tbl %>% as.matrix() -> cat_tbl
+      rownames(cat_tbl) <- union_all #grid_ptn 
+      cat_tbl
+      # cat_tbl の名前部分に NA が含まれていれば "NA" に変換
+      names(cat_tbl) <- ifelse(is.na(names(cat_tbl)), "NA", names(cat_tbl))
+      
+      rownames(t(cat_tbl)) -> rnames
+      t(cat_tbl) %>% as.tibble() %>% mutate(ID=rnames,IDn=1:length(rnames)) %>% # ★t(cat_tbl) %>% as.tibble() でエラー
+        arrange(desc(union_all[1])) %>% 
+        select(IDn) %>% unlist %>% 
+        setNames(NULL) -> order_vec
+     
+      t(cat_tbl)[order_vec,] %>%  
+      #t(cat_tbl) %>% 
+        mosaic(shade = TRUE,rot_labels = c(0, 0),
+                            margins=c(left=12,top=5),just_labels=c(left="right",top="left"))
+      
+    })
+    
+    
+    # GridAnswerG CA
+    output$GridAnswerG_CA <- renderPlot({
+      # grid_ptn <- c("A","B","C","D","E","NA") # 仮のパターン
+      # selected_vars <- input$variables 
+      # count_categories <- function(x) {
+      #   table(factor(x, levels = grid_ptn,#c("++", "+", "-", "--", "DK", "無回答"), 
+      #                exclude = NULL))
+      # }
+      # 
+      # # df の 1 列目から 20 列目の各列ごとにカテゴリを集計
+      # category_count_tbl <- data_for_plot() %>%
+      #   dplyr::summarise(across(selected_vars, ~ count_categories(.))) 
+      # 
+      # category_count_tbl %>% as.matrix() -> cat_tbl
+      # rownames(cat_tbl) <- grid_ptn #c("++","+","-","--","DK","無回答")
+      # cat_tbl
+
+      selected_vars <- input$variables
+      #browser()
+      vectors <- map(selected_vars, ~ {
+        data_for_plot() %>% select(selected_vars) %>% 
+          count(!!sym(.x)) %>%  # 選択した列ごとにカウント
+          pull(1)               # 最初の列のユニークな値を取得
+      })
+      
+      union_all <- reduce(vectors, union)
+      
+      count_categories <- function(x) {
+        table(factor(x, levels = union_all, exclude = NULL))
+      }
+      
+      # df のselected_varsの各列ごとにカテゴリを集計
+      category_count_tbl <- data_for_plot() %>%
+        dplyr::summarise(across(selected_vars, ~ count_categories(.))) 
+      
+      category_count_tbl %>% as.matrix() -> cat_tbl
+      rownames(cat_tbl) <- union_all #grid_ptn 
+      cat_tbl
+      
+            
+      res.CA <- FactoMineR::CA(t(cat_tbl))
+      
+    })
+    
+
+# タイムスタンプを取得    
+    output$timestamp <- renderText({
+      file_info <- file.info("app.R")  # ファイル情報を取得
+      timestamp <- file_info$mtime     # 最終更新日時
+      paste("app.Rの最終更新:", format(timestamp, "%Y-%m-%d %H:%M:%S"))
+    })
+
+# 度数分布表
     output$simple_table <- DT::renderDataTable({ #renderTable({#
       table(data_for_plot()[,input$select_input_data_for_hist]) -> tmp
       round(100*prop.table(tmp),1) -> tmp2
